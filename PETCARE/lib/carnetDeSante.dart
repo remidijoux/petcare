e3587346722314e728beb71ba5ed9c89201b00d8
juliaoto <79646127+juliaoto@users.noreply.github.com>
2023-05-25 15:28:29 +0200

    cs

--- PETCARE/lib/carnetDeSante.dart
+++ PETCARE/lib/carnetDeSante.dart
@@ -6,25 +6,33 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Carnet de santé',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Menu déroulant'),
+        ),
+        body: Center(
+          child: DropdownButton<String>(
+            value: 'Type animal', // Option sélectionnée par défaut
+            items: <String>[
+              'Type animal',
+              'Chien',
+              'Chat',
+              'Oiseau',
+              'Rongeur',
+            ].map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              print('Option sélectionnée : $newValue');
+            },
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Carnet de santé'),
     );
   }
 }
@@ -60,6 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
