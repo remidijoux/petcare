86987089b161a84035b0af3d39352046567027cb
juliaoto <79646127+juliaoto@users.noreply.github.com>
2023-05-30 11:32:09 +0200

    propre

--- PETCARE/lib/carnetDeSante.dart
+++ PETCARE/lib/carnetDeSante.dart
@@ -4,177 +4,24 @@ void main() {
   runApp(const MyApp());
 }
 
-class TypeAnimal extends StatefulWidget {
-  @override
-  _DropdownMenuWidgetState createState() => _DropdownMenuWidgetState();
-}
-
-class _DropdownMenuWidgetState extends State<TypeAnimal> {
-  String selectedValue = 'Chien';
-
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: selectedValue,
-      onChanged: (String? newValue) {
-        setState(() {
-          selectedValue = newValue!;
-        });
-      },
-      items: <String>['Chien', 'Chat', 'Oiseau', 'Rongeur',]
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-  }
-}
-
-class SexeAnimal extends StatefulWidget {
-  @override
-  _DropdownMenuWidgetState2 createState() => _DropdownMenuWidgetState2();
-}
-
-class _DropdownMenuWidgetState2 extends State<SexeAnimal> {
-  String selectedValue = 'Mâle';
-
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: selectedValue,
-      onChanged: (String? newValue) {
-        setState(() {
-          selectedValue = newValue!;
-        });
-      },
-      items: <String>['Mâle', 'Femelle',]
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-  }
-}
-
-class DateOfBirthPicker extends StatefulWidget {
-  @override
-  _DateOfBirthPickerState createState() => _DateOfBirthPickerState();
-}
-
-class _DateOfBirthPickerState extends State<DateOfBirthPicker> {
-  late DateTime selectedDate;
-
-  void initState(){
-    super.initState();
-    selectedDate = DateTime.now();
-  }
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: selectedDate ?? DateTime.now(),
-      firstDate: DateTime(1900),
-      lastDate: DateTime.now(),
-    );
-
-    if (pickedDate != null && pickedDate != selectedDate) {
-      setState(() {
-        selectedDate = pickedDate;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        _selectDate(context);
-      },
-      child: Container(
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-        ),
-        child: Text(
-          selectedDate != null
-              ? '${selectedDate.day}/${selectedDate.month}/${selectedDate.year}'
-              : 'Sélectionner une date',
-          style: TextStyle(fontSize: 16),
-        ),
-      ),
-    );
-  }
-}
-
-class AnimalExam {
-  final String name;
-  final String result;
-
-  AnimalExam({required this.name, required this.result});
-}
-
-class AnimalDetails extends StatefulWidget {
-  @override
-  _AnimalDetailsState createState() => _AnimalDetailsState();
-}
-class _AnimalDetailsState extends State<AnimalDetails> {
-  List<AnimalExam> exams = [];
-
-  void addExam(String name, String result) {
-    setState(() {
-      exams.add(AnimalExam(name: name, result: result));
-    });
-  }
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Carnet de santé'),
-        ),
-        body: Column(
-          children: [
-            TypeAnimal(), // appel du menu déroulant type animal
-            // Widget TextField pour nom de l'animal
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Nom de l\'animal',
-              ),
-              onChanged: (value) {
-              },
-            ),
-            SexeAnimal(), // appel du menu déroulant sexe animal
-            DateOfBirthPicker(), // appel de la methode pour la date de naissance
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Nom du vétérinaire',
-              ),
-              onChanged: (value) {
-              },
-            ),
-          ],
-        ),
+      title: 'Carnet de santé',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'Carnet de santé'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -184,72 +31,121 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String? _selectedAnimal;
+  String? _selectedGender;
+  DateTime? _selectedBirthDate;
+  String? _vetName;
+  DateTime? _lastVisitDate;
+  DateTime? _selectedLastVisitDate;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Column(
+              children: <Widget>[
+                DropdownButton<String>(
+                  value: _selectedAnimal,
+                  hint: const Text('Sélectionnez un animal'),
+                  items: <String>['Chien', 'Chat', 'Rongeur', 'Oiseau'].map((
+                      String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedAnimal = newValue;
+                    });
+                  },
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nom de l\'animal',
+                  ),
+                ),
+                DropdownButton<String>(
+                  value: _selectedGender,
+                  hint: const Text('Sélectionnez le sexe'),
+                  items: <String>['Femelle', 'Mâle'].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedGender = newValue;
+                    });
+                  },
+                ),
+                TextButton(
+                  onPressed: () async {
+                    final DateTime? picked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime.now(),
+                    );
+                    if (picked != null) {
+                      setState(() {
+                        _selectedBirthDate = picked;
+                      });
+                    }
+                  },
+                  child: const Text('Sélectionnez la date de naissance'),
+                ),
+                Text(
+                  _selectedBirthDate != null
+                      ? 'Date de naissance : ${_selectedBirthDate!.day}/${_selectedBirthDate!.month}/${_selectedBirthDate!.year}'
+                      : 'Date de naissance non sélectionnée',
+                ),
+
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nom du vétérinaire',
+                  ),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    final DateTime? picked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime.now(),
+                    );
+                    if (picked != null) {
+                      setState(() {
+                        _lastVisitDate = picked;
+                      });
+                    }
+                  },
+                  child: const Text(
+                      'Sélectionnez la date de la dernière visite chez le vétérinaire'),
+                ),
+                Text(
+                  _selectedLastVisitDate != null
+                      ? 'Dernière visite chez le vétérinaire : ${_selectedLastVisitDate!.day}/${_selectedLastVisitDate!.month}/${_selectedLastVisitDate!.year}'
+                      : 'Date de dernière visite non sélectionnée',
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
-
-
-
