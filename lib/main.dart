3cfc3634517120f331d4d5bf004bf639f0abe0dc
unknown <remidijoux@gmail.com>
2023-06-05 19:11:10 +0200

    BLOG et question

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'FindVet.dart';
+import 'Blog.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,7 +56,10 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void navigateToBlog() {
-    // page du blog
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => BlogPage()),
+    );
   }
 
   @override
