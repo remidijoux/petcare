dfbc7b1cc11d1f47d693883f84aadf9dc7774eb8
Ahmed Brhili <brhiliahmed@gmail.com>
2023-06-05 16:37:45 +0200

    implement data carnet de sante fom in firestore cloud collection

---
+++ lib/widgets/submit_btn.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class  submitBtn  extends StatelessWidget {
+  const submitBtn  ({Key? key , required this.onPress}) : super(key: key);
+
+
+  final VoidCallback onPress;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0),
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          minimumSize: const Size(200, 50),
+        ),
+        onPressed: onPress,
+
+        child: Text("Submit".toUpperCase(),
+            style: TextStyle(fontWeight: FontWeight.bold)
+        ),
+      ),
+    );
+  }
+}
